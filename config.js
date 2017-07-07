525ae858a8459d526b3116bc2a82d44c892067bb
夏目贵志 <6@9i3.cn>
2017-07-07 14:55:49 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 var _config = {
     blog_name       : '用于演示的博客',         // 博客名称
-    owner           : 'lifesinger',           // github 用户名
-    repo            : 'lifesinger.github.com',// github 中对应项目名
+    owner           : 'xiamuguizhi',           // github 用户名
+    repo            : 'xiamuguizhi.github.io/xiamu',// github 中对应项目名
     duoshuo_id      : 'hello1234',            // 在第三方评论插件多说申请站点的子域名
-    // access_token : 'abcde'+'fghijk',       // 请求量大时需要在 github 后台单独设置一个读取公开库的 token, 注意将token 拆成两个字符串，否则会被系统自动删除掉
+    // access_token : '4ffd17c054e91ecb3f0778'+'cf5d7ae70ebbba6ef1',       // 请求量大时需要在 github 后台单独设置一个读取公开库的 token, 注意将token 拆成两个字符串，否则会被系统自动删除掉
     per_page        : '15'                    // 默认一页显示几篇文章
 }
 
